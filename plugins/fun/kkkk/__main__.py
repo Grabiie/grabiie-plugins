7358063d351835ee1f11503e55fce5c3bb49a46f
Gabriel José <42703366+Grabiie@users.noreply.github.com>
2022-07-07 22:55:13 -0300

    Update __main__.py

--- plugins/fun/kkkk/__main__.py
+++ plugins/fun/kkkk/__main__.py
@@ -4,12 +4,12 @@
 from userge import Message, userge
 
 @userge.on_cmd("k|K", about={"header": "risada curta"}, allow_via_bot=False)
-async def k_(message: Message):
+async def k(message: Message):
     out_str =f"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
     await message.edit(out_str)
 
 @userge.on_cmd("kkk|KKK", about={"header": "risada  longa"}, allow_via_bot=False)
-async def kkk_(message: Message):
+async def kkk(message: Message):
     out_str = f"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
     await message.edit(out_str)
 
